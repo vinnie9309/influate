06809a972b628629b7fb1bb0aebcef82fbb66d8d
Viktor Georgiev <vgeorgiev1@icloud.com>
2024-04-27 09:18:51 +0300

    add auth with basic styles

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,14 @@
-import "./globals.css";
+import "./globals.css"
 
 export const metadata = {
   title: "shortmemory.com",
-  description: "where you keep your short memory",
-};
+  description: "where you keep your short memory"
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,5 +4,5 @@ export default function Home() {
       <h1>Welcome to shortmemory.com</h1>
       <p>where you keep your short memory</p>
     </div>
-  );
+  )
 }
